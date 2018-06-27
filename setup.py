7b0d34724caa49d370024fe86b9d92cad54eb0e2
Natsume Kota <kota99949@gmail.com>
2018-06-28 02:01:51 +0900

    [Fix] fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='pgpipelines',
     version='0.0.1',
-    description='# Pgpipeline: postgres pipeline for Scrapy',
+    description='postgres pipeline for Scrapy',
     long_description=open('README.md').read(),
     author='kota999',
     author_email='kota99949@gmail.com',
